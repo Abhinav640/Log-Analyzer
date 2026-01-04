4fff317a19456d359020cb02ca42174257cb64fd
Abhinav640 <pathak.abhinav640@gmail.com>
2026-01-04 23:06:42 +0530

    added log file formatter

--- analyze.py
+++ analyze.py
@@ -28,11 +28,3 @@ def log_format(orignal_file, formatted_file):
             cor_file.write(line + "\n")
 
 
-def main():
-    orignal_file = "sample.log"
-    formatted_file = "formated.log"
-    log_format(orignal_file, formatted_file)
-
-
-if __name__ == "__main__":
-    main()
