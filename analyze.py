48184122fa99b3d90de435b4cf88da3cabfca552
Abhinav640 <pathak.abhinav640@gmail.com>
2026-01-08 00:10:32 +0530

    Added args_pars

--- analyze.py
+++ analyze.py
@@ -1,50 +1,65 @@
 import re
 import pdb
 
-def log_format(orignal_file, formatted_file):
+class LogAnalyzer:
+    def __init__(self,orignal_file, formatted_file, error_type):
+        self.orignal_file = orignal_file
+        self.formatted_file = formatted_file
+        self.error_type = error_type
+        
 
-    log_data = []    
-    current_log = ""
-    timestamp_pattern = r'^(\[\d{4}-\d{2}-\d{2})'
+    def log_format(self):
 
-    with open (orignal_file, "r") as file:
-        # pdb.set_trace()
-        log_file = file.readlines()
-        for lines in log_file:
-            lines = lines.strip()
+        log_data = []    
+        current_log = ""
+        timestamp_pattern = r'^(\[\d{4}-\d{2}-\d{2})'
 
-            if re.match(timestamp_pattern, lines):
-                if current_log != "":
-                    log_data.append(current_log)
-                current_log = lines
-            else:
-                current_log = current_log + " " + lines
+        with open (self.orignal_file, "r") as file:
+            # pdb.set_trace()
+            log_file = file.readlines()
+            for lines in log_file:
+                lines = lines.strip()
 
-        if current_log != "":
-            log_data.append(current_log)
+                if re.match(timestamp_pattern, lines):
+                    if current_log != "":
+                        log_data.append(current_log)
+                    current_log = lines
+                else:
+                    current_log = current_log + " " + lines
+
+            if current_log != "":
+                log_data.append(current_log)
+                    
+        with open(self.formatted_file, "w") as cor_file:
+            for line in log_data:
+                updated_line = line.replace("  ", " ")
+                cor_file.write(updated_line + "\n")
+
+    def log_parser(self):
+        error_types = {}
+        with open (self.formatted_file, "r") as file:
+            log_file = file.readlines()
+            for lines in log_file:
+                error = lines.split(" ")[2].upper()
+                if error not in error_types:
+                    error_types[error] = 1
+                else:
+                    error_types[error] += 1
                 
-    with open(formatted_file, "w") as cor_file:
-        for line in log_data:
-            updated_line = line.replace("  ", " ")
-            cor_file.write(updated_line + "\n")
+        return error_types
 
-def log_parser(formatted_file):
-    error_type = {}
-    with open (formatted_file, "r") as file:
-        log_file = file.readlines()
-        for lines in log_file:
-            error = lines.split(" ")[2]
-            if error not in error_type:
-                error_type[error] = 1
-            else:
-                error_type[error] += 1
-            
-    return error_type
-
-def log_list(formatted_file, error_type):
-    with open (formatted_file, "r") as file:
-        log_file = file.readlines()
-        for lines in log_file:
-            if error_type in lines:
-                print(lines)
+    def log_list(self):
+        error_list = []
+        with open (self.formatted_file, "r") as file:
+            log_file = file.readlines()
+            for lines in log_file:
+                error_type = str(self.error_type).upper()                
+                if error_type in lines:                    
+                    error_list.append(lines) 
+        return error_list
     
+    def act(self):
+        error_count = self.log_parser()[self.error_type.upper()]
+        if error_count >= 10:
+            return f"high amount of {self.error_type} - log count: {error_count}"
+        
--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from analyze import log_format, log_parser, log_list
+from analyze import LogAnalyzer
+
+import argparse
 
 def main():
-    orignal_file = "sample.log"
-    formatted_file = "formated.log"
-    error_type = "debug"
-    log_format(orignal_file, formatted_file)
-    print(log_parser(formatted_file))
-    log_list(formatted_file, error_type.upper())
+    parser = argparse.ArgumentParser(description= "This is a LogAnalyser that assis you with logs")
+    parser.add_argument("original_file", metavar="RAW_LOG_FILE", type=str, help= "path to your raw log file")
+    parser.add_argument("formatted_file", metavar="UPDATED_LOG_FILE", type=str, help= "this file gets created after formating the raw file")
+    parser.add_argument("error_type", metavar="ERROR_TYPE", type=str, help= "enter the error type you are looking for")
+    parser.add_argument("analyzer", metavar="APP_FUNCTION", type=str, help= "enter what are you looking for")
+    
+    args = parser.parse_args()
 
+    original_file = args.original_file
+    formatted_file = args.formatted_file
+    error_type = args.error_type
+
+
+    analyze = LogAnalyzer(original_file, formatted_file, error_type)
+
+    app_function = args.analyzer
+
+    analyze.log_format()
+    if app_function == "log_parser":
+        print(analyze.log_parser())
+    elif app_function == "log_list":
+        print(analyze.log_list())
+    elif app_function == "act":
+        print(analyze.act())
+        
 
 if __name__ == "__main__":
     main()
